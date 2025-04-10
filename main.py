024a7180ce46e447cbb87892c072828d8a76f299
lrkc1756 <142984076+lrkc1756@users.noreply.github.com>
2025-04-10 11:49:16 +0200

    fix: app running. Problem: everything is tiny

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ app = create_app()
 
 #runs webserver if this file is run directly
 if __name__ == '__main__':
-    app.run(host='192.168.1.103', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
+
     
     
